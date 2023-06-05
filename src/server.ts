69a7ed3d5d4cb2fb36d125086aaefce787761bcf
nahid9764 <nahidhossain9764@gmail.com>
2023-06-05 07:25:30 +0600

    Project setup done

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function bootstrap() {
+	try {
+		await mongoose.connect(config.db_url as string);
+		console.log("mongo server connected!");
+		app.listen(config.port, () => {
+			console.log(`Server listening on port ${config.port}`);
+		});
+	} catch (err) {
+		console.log(err);
+	}
+}
+bootstrap();
