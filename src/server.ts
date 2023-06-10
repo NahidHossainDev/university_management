1b2f2492dccc2eadd59ecbdc2fc6be06183b7fe8
nahid9764 <nahidhossain9764@gmail.com>
2023-06-10 12:14:18 +0600

    winston logger added

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { logger } from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.db_url as string)
-    console.log('mongo server connected!')
+    logger.info('mongo server connected!')
     app.listen(config.port, () => {
-      console.log(`Server listening on port ${config.port}`)
+      logger.info(`Server listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(err)
+    logger.error(err)
   }
 }
 bootstrap()
---
+++ src/shared/logger.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+import path from 'path'
+import winston from 'winston'
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+
+const errorLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+export { errorLogger, logger }
